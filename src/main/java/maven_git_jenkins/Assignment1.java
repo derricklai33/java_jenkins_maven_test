4d6718afada94fe9ad8de72875c04c77c41b86f4
derrick <joonyeapderrick.lai@cognizant.com>
2021-07-22 15:55:25 +1000

    Initial commit

---
+++ src/main/java/maven_git_jenkins/Assignment1.java
@@ -0,0 +1,17 @@
+package maven_git_jenkins;
+
+public class Assignment1 {
+	
+	int a = 10;
+	double b = 20.54;
+	char c = 'x';
+	String d = "I am the best";
+	
+	public static void main(String[] args) {
+		Assignment1 result = new Assignment1();
+		System.out.println(result.a);
+		System.out.println(result.b);
+		System.out.println(result.c);
+		System.out.println(result.d);
+	}
+}
